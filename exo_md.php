d32b3a694c52bd7b680355d3ced614bf394f7cdd
Nitha Thghm <t.nithachan@gmail.com>
2022-09-06 09:12:49 +0200

    First commit

--- exo_md.php
+++ exo_md.php
@@ -1,6 +1,25 @@
 <?php
 
-class exo_md
+/** Exercice 4 :
+ *
+ *Créez une classe nommée form représentant un formulaire HTML.
+ *Le constructeur doit créer le code d’en-tête du formulaire en utilisant les éléments <form> et <fieldset>.
+ *Une méthode setText() doit permettre d’ajouter une zone de texte. Une méthode setSubmit() doit permettre d’ajouter un bouton d’envoi.
+ *Les paramètres de ces méthodes doivent correspondre aux attributs des éléments XHTML correspondants.
+ * La méthode getForm() doit retourner tout le code XHTML de création du formulaire. Créez des objets form, et ajoutez-y deux zones de texte et un bouton d’envoi.
+ * Testez l’affichage obtenu.
+ */
+
+class form
 {
 
+    public function __construct(){
+
+    }
+
+    public function setText()
+    {
+    }
+
+    public function set
 }
